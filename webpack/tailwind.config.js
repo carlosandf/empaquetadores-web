b3bde3510164fa60e1590c729d6bb793d4150da2
carlosandf <carlos.andres.ff2912@gmail.com>
2023-11-02 14:36:44 -0500

    Update tailwind config

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.html', './src/**/*.css'],
+  content: [
+    './dist/*.html',
+    './src/**/*.{js,css}'
+  ],
   theme: {
     extend: {},
   },
